5f279b5a744207405df345e9b2ce0285ffef944e
mohamed <matassi.mohamed89@gmail.com>
2023-09-29 11:22:03 +0100

    Completed assignment

--- src/Clock.js
+++ src/Clock.js
@@ -4,9 +4,14 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const timerID = setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+    // returning a cleanup function to the setInterval from running in the background
+    return function cleanup() {
+      clearInterval(timerID);
+    };
   }, []);
 
   return <div>{time.toString()}</div>;
